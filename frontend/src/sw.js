add2875f40e0e3e1c37d0ff41c45ab7d2f36c184
Max <max@example.com>
2026-03-18 22:53:58 +0400

    fix: clear all caches on SW activate to prevent white screens

--- frontend/src/sw.js
+++ frontend/src/sw.js
@@ -85,3 +85,16 @@ self.addEventListener("message", function (event) {
     self.skipWaiting();
   }
 });
+
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
