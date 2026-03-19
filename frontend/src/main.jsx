696c89c63caf86e452bfa837caa30044fa186671
Max <max@example.com>
2026-03-19 21:25:52 +0400

    Add service worker cleanup on load to fix white screen on refresh

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,10 @@
+// Unregister broken service workers then reload
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(reg => reg.unregister());
+  });
+}
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
