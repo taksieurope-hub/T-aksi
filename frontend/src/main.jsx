598316cc1e0ddd06e6c5b73cb5910a4327f76473
Max <max@example.com>
2026-03-21 01:02:03 +0400

    fix: remove nested BrowserRouter causing white screen on refresh

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { LanguageProvider } from './i18n/LanguageContext';
 import App from './App';
 import './index.css';
 
@@ -17,10 +15,6 @@ if ('serviceWorker' in navigator) {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LanguageProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </LanguageProvider>
+    <App />
   </React.StrictMode>
 );
